9192d1d9a3731b06324a63232987326b32fb27ec
Fiifo <131212518+Fiifo@users.noreply.github.com>
2024-08-17 07:28:25 -0300

    Basic 16x16 grid working (no outline yet)

---
+++ script.js
@@ -0,0 +1,31 @@
+function test(){
+    window.addEventListener('mouseover', (e) => {
+        const {
+          clientX: x,
+          clientY: y
+        } = e
+        const elementMouseIsOver = document.elementFromPoint(x, y);
+        // console.log(elementMouseIsOver)
+        elementMouseIsOver.style.opacity = "1";
+      })
+}
+function createBoards(lineL){
+    var container = document.querySelector(".board")
+    for(var j = 0; j < 16; j++){
+        var line = document.createElement("div");
+        line.classList.add("line");
+        for(var i = 0; i < 16; i++){
+            var box = document.createElement("span");
+            box.id = (j*100) + i;
+            box.textContent = "a "
+            box.addEventListener("mouseover", test)
+            line.appendChild(box);
+        }
+        container.appendChild(line);
+    }
+}
+
+
+createBoards(2);
+
+
